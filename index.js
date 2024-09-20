aa7fe2f956172688db7a9193bc7d91de25770d95
Hany Medhat <hany.medhat24@gmail.com>
2024-09-20 16:07:36 +0300

    Update Docker configuration, add .env file, and set up Express server with MongoDB integration
    - Updated the `.gitignore` file to include the `.env` file, which stores environment variables.
    - Modified the `docker-compose.yml` file to update MongoDB and Mongo Express configurations, including changing the root username and password, and updating the environment variables accordingly.
    - Added a new `index.js` file to set up an Express server with MongoDB integration, including routes for authentication.
    - Updated the `package.json` file to include new dependencies (`config`, `dotenv`, `mongod`) and added scripts for starting the server in different modes.
    - Created a new `auth.js` router file to handle authentication routes.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const authRouter = require("./routers/auth");
+const startup = "/api";
+app.use(express.json());
+app.use(`${startup}/auth`, authRouter);
+// app.use(express.static())
+app.get(startup, (req, res) => {
+  res.send("Hello world");
+});
+
+mongoose
+  .connect(process.env.URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ routers/auth.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("Hello Auth Model");
+});
+router.post("/signup", (req, res) => {
+    const {name,email,password} = req.body;
+    
+})
+
+router.post("/login", (req, res) => {
+    const {email,password} = req.body;
+    
+})
+
+module.exports = router;
