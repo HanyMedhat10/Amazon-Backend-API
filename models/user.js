4db6afa251f9b6c0ba540ccf35225f69bc988bff
Hany Medhat <hany.medhat24@gmail.com>
2024-09-21 08:48:12 +0300

    Update User Model and Authentication Routes
    - Added email validation to the User model.
    - Implemented password hashing using bcryptjs for secure storage.
    - Updated the signup route to check for existing users and validate password length.
    - Implemented the login route to verify user credentials and compare hashed passwords.
    - Fixed minor formatting issues in the User model and authentication routes.
    
    This commit enhances the security and functionality of user authentication by implementing proper password hashing and validation checks.

--- models/user.js
+++ models/user.js
@@ -1,46 +1,49 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+      unique: true,
+    email: true,
+    validate: {
+      validator: (value) => {
+        const re =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return value.match(re);
+      },
+      message: "Please enter a valid email address",
     },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        Validate: {
-            validator: (value) => {
-                const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                return value.match(re);
-            },
-            message: "Please enter a valid email address"
-        } 
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 8,
-    },
-    address: {
-        type: String,
-        default: "",
-    },
-    type: {
-        type: String,
-        default: "user",
-    },
-    // tokens: [{
-    //     token: {
-    //         type: String,
-    //         required: true
-    //     }
-    // }],
-    // avatar: {
-    //     type: Buffer
-    // }
-})
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+  },
+  address: {
+    type: String,
+    default: "",
+  },
+  type: {
+    type: String,
+    default: "user",
+  },
+  // tokens: [{
+  //     token: {
+  //         type: String,
+  //         required: true
+  //     }
+  // }],
+  // avatar: {
+  //     type: Buffer
+  // }
+});
 
 const User = mongoose.model("User", userSchema);
-module.exports = User
+module.exports = User;
--- routers/auth.js
+++ routers/auth.js
@@ -1,16 +1,52 @@
 const express = require("express");
 const router = express.Router();
-
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 router.get("/", (req, res) => {
     res.send("Hello Auth Model");
 });
-router.post("/signup", (req, res) => {
-    const { name, email, password } = req.body;
-    
+router.post("/signup", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const existingUser =await User.findOne({ email })
+        if (existingUser) {
+            return res.status(422).json({ error: "User already exists" });
+        }
+        if (password.length < 6) {
+            return res
+                .status(422)
+                .json({ error: "Password should be at least or Bigger than 6 characters" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 12);
+        let user = new User({
+          name: name,
+          email: email,
+          password: hashedPassword,
+        });
+        user =  await user.save();
+        res.status(201).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
 })
 
 router.post("/login", (req, res) => {
     const {email,password} = req.body;
+    let user = User.findOne({email});
+    if (!user) {
+        return res
+        .status(422)
+        .json({ error: "Invalid Email or Password" });
+    }
+    const isMatch = bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        return res
+        .status(422)
+        .json({ error: "Invalid Email or Password" });
+    }
+    // ??? I want add a token and send it to the user
+    res.json({ message: "Login Successful" });
     
 })
 
