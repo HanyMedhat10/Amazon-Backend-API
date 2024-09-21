a461e5dddd395b7b610d610ca9302d56178d5771
Hany Medhat <hany.medhat24@gmail.com>
2024-09-21 17:04:49 +0300

    Add authentication middleware and update user routes
    - Created a new middleware `auth.js` to handle JWT verification and user authentication.
    - Updated the `auth.js` router to include protected routes for fetching user data and user profile.
    - Added a new route to check the validity of a JWT token.
    - Updated the sign-in and sign-up routes to include JWT token generation and password hashing.
    - Added new custom words to the VSCode settings for spell checking.

---
+++ middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const auth = (req, res, next) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res.status(401).send("No token,Access Denied");
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res
+        .status(401)
+        .send("Token verification failed,authorization Denied");
+    }
+    req.user = decoded.id;
+    req.token = token;
+    next();
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
--- routers/auth.js
+++ routers/auth.js
@@ -3,57 +3,80 @@ const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-router.get("/", (req, res) => {
-    res.send("Hello Auth Model");
+// get user data
+router.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user);
+  res.json({ ...user._doc, token: req.token });
 });
 router.post("/signup", async (req, res) => {
-    try {
-        const { name, email, password } = req.body;
-        const existingUser =await User.findOne({ email })
-        if (existingUser) {
-            return res.status(422).json({ error: "User already exists" });
-        }
-        if (password.length < 6) {
-            return res
-                .status(422)
-                .json({ error: "Password should be at least or Bigger than 6 characters" });
-        }
-        const hashedPassword = await bcrypt.hash(password, 12);
-        let user = new User({
-          name: name,
-          email: email,
-          password: hashedPassword,
-        });
-        user =  await user.save();
-        res.status(201).json(user);
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: error.message });
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(422).json({ error: "User already exists" });
     }
-})
+    if (password.length < 6) {
+      return res
+        .status(422)
+        .json({
+          error: "Password should be at least or Bigger than 6 characters",
+        });
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    let user = new User({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+    user = await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
 
 router.post("/signin", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        
-        let user =await User.findOne({email});
-        if (!user) {
-            return res
-            .status(422)
-            .json({ error: "Invalid Email or Password" });
-        }
-        const isMatch = bcrypt.compare(password, user.password)
-        if (!isMatch) {
-            return res
-            .status(422)
-            .json({ error: "Invalid Email or Password" });
-        }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-        return res.json({ token, ...user._doc });
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(422).json({ error: "Invalid Email or Password" });
     }
-    
-})
+    const isMatch = bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(422).json({ error: "Invalid Email or Password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    return res.json({ token, ...user._doc });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/tokenIsValid:", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if (!verified) return res.json(false);
+    const user = await User.findById(verified.id);
+    if (!user) return res.json(false);
+    return res.json(true);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Get user Profile
+router.get("/profile", async (req, res) => {
+  try {
+    const user = await User.findById(req.user);
+    res.json({ ...user._doc, token: req.token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 module.exports = router;
