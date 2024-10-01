8fafd22cd174f65ba4d63f32e3adefd848e04db0
Hany Medhat <hany.medhat24@gmail.com>
2024-10-02 02:26:46 +0300

    Add search functionality and improve error handling in product router
    - Implemented a new route `/get-products/search/:name` to allow searching products by name using a case-insensitive regex.
    - Enhanced the existing `/get-products` route to return all products if no category is specified.
    - Improved error handling by logging errors and returning a 500 status with the error message.

--- routers/product.js
+++ routers/product.js
@@ -6,9 +6,24 @@ router.get("/get-products", async (req, res) => {
   try {
     const category = req.query.category;
     console.log(category);
-    const products = await Product.find({ category });
+    const products = category
+      ? await Product.find({ category })
+      : await Product.find();
     res.send(products);
-} catch (error) {
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+});
+router.get("/get-products/search/:name", async (req, res) => {
+  try {
+    const name = req.params.name;
+    console.log(name);
+    const products = await Product.find({
+      name: { $regex: name, $options: "i" },
+    });
+    res.send(products);
+  } catch (error) {
     console.error(error);
     res.status(500).send({ error: error.message });
   }
