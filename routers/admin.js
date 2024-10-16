6a4d3f00ca35e5539b863f8cc19a24c005e42422
Hany Medhat <hany.medhat24@gmail.com>
2024-10-16 20:44:46 +0300

    Refactor admin router and add analytics endpoint
    - Improved readability and consistency by refactoring the admin router code.
    - Added a new `/analytics` endpoint to calculate total earnings and category-wise earnings.
    - Implemented the `eachCategoryEarings` function to calculate earnings for each category.
    - Ensured all responses are returned with `return` statements for clarity and consistency.

--- routers/admin.js
+++ routers/admin.js
@@ -49,18 +49,52 @@ router.get("/get-product/:id", admin, async (req, res) => {
     res.status(500).send({ error: error.message });
   }
 });
-router.get('/get-orders', admin, async (req, res) => {try {
-  
-  const orders = await Order.find();
-  res.send(orders)
-  
-} catch (error) {
-  console.error(error);
-  res.status(500).send({ error: error.message });
-}});
+router.get("/get-orders", admin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    res.send(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: error.message });
+  }
+});
+/* 
+      'Mobiles',
+      'Essentials',
+      'Appliances',
+      'Books',
+      'Fashion',
+*/
+//
+router.get("/analytics", admin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+    let totalEarnings = 0;
+    for (let i = 0; i < orders.length; i++) {
+      totalEarnings += orders[i].totalPrice;
+    }
+    // ??because not every category has products
+    // const categories = await Product.find().distinct("category");
+    const categories = [
+      "Mobiles",
+      "Essentials",
+      "Appliances",
+      "Books",
+      "Fashion",
+    ];
+    let categoryEarings = {};
+    for (let i = 0; i < categories.length; i++) {
+      categoryEarings[categories[i]] = await eachCategoryEarings(categories[i]);
+    }
+    return res.send({ totalEarnings, ...categoryEarings });
+  } catch (error) {
+    console.error(error);
+    return  res.status(500).send({ error: error.message });
+  }
+});
 
 // update products by ID
-router.patch('/update-order/:id', admin, async (req, res) => {
+router.patch("/update-order/:id", admin, async (req, res) => {
   try {
     const id = req.params.id;
     const { status } = req.body;
@@ -71,10 +105,9 @@ router.patch('/update-order/:id', admin, async (req, res) => {
     return res.json(order);
   } catch (error) {
     console.error(error);
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 });
-
 
 // delete products by ID
 router.delete("/delete-product/:id", admin, async (req, res) => {
@@ -84,9 +117,25 @@ router.delete("/delete-product/:id", admin, async (req, res) => {
     return res.json(product);
   } catch (error) {
     console.error(error);
-    res.status(500).send({ error: error.message });
+    return res.status(500).send({ error: error.message });
   }
 });
 
+async function eachCategoryEarings(category) {
+  const orders = await Order.find({
+    "products.product.category": category,
+  }).select({
+    "products.product.price": true,
+    "products.quantity": true,
+  });
+  let totalAmount = 0;
+  for (let i = 0; i < orders.length; i++) {
+    for (let j = 0; j < orders[i].products.length; j++) {
+      totalAmount +=
+        orders[i].products[j].quantity * orders[i].products[j].product.price;
+    }
+  }
+  return totalAmount;
+}
 
 module.exports = router;
